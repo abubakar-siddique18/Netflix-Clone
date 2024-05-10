a61ef10a8b76feb92bf2225cfc0f1270788d336a
Chappali Shaik Abubakar Siddique <166218344+my-projects-demo@users.noreply.github.com>
2024-05-10 18:40:23 +0530

    final commit

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5_&#uv7ry+zs5)*7^uqq9nmxmh*3zcy-gu_!npsdxvwkbjc$$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
