8da6b2871a92f6b7624d22192f47adfba7928c56
Chappali Shaik Abubakar Siddique <166218344+my-projects-demo@users.noreply.github.com>
2024-05-11 00:14:28 +0530

    update access

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5_&#uv7ry+zs5)*7^uqq9nmxmh*3zcy-gu_!npsdxvwkbjc$$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
