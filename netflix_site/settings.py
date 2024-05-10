15e4b2c7d62276db283a2b36dda5d3f5e12fe3a8
Chappali Shaik Abubakar Siddique <166218344+my-projects-demo@users.noreply.github.com>
2024-05-10 23:19:56 +0530

    last commit

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5_&#uv7ry+zs5)*7^uqq9nmxmh*3zcy-gu_!npsdxvwkbjc$$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 
